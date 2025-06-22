0bc2e6dd53583912f28de3e74ee6126aec773352
Bazhenov Yan <bazhenovyan@yandex.ru>
2025-06-23 01:57:47 +0400

    Added first test. Change

--- src/test/java/ZipCodeTest.java
+++ src/test/java/ZipCodeTest.java
@@ -11,17 +11,12 @@ public class ZipCodeTest {
     @Test
     public void test() {
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         driver.get("https://sharelane.com/cgi-bin/register.py");
-        driver.findElement(By.name("zip_code")).sendKeys("11111");
+        driver.findElement(By.name("zip_code")).sendKeys("1111");
         driver.findElement(By.cssSelector("[value=Continue]")).click();
-        driver.findElement(By.name("first_name")).sendKeys("Test");
-        driver.findElement(By.name("last_name")).sendKeys("User");
-        driver.findElement(By.name("email")).sendKeys("test123@test.test");
-        driver.findElement(By.name("password1")).sendKeys("qwerty123456");
-        driver.findElement(By.name("password2")).sendKeys("qwerty123456");
-        driver.findElement(By.cssSelector("[value=Register]")).click();
-        String confirmationMessage = driver.findElement(By.cssSelector("[class=confirmation_message]")).getText();
-        Assert.assertEquals(confirmationMessage, "Account is created!");
+        String errorMessage = driver.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
         driver.quit();
     }
 }
